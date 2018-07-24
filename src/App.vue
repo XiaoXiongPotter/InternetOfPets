2a3c53ae8f870906ccdd88f387f8b82ef1b2202f
wanghaiyang <619747158@qq.com>
2018-07-24 15:40:28 +0800

    header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-
+    <v-header></v-header>
   	<router-view></router-view>
-  <footernav></footernav>
+  <v-foot></v-foot>
   </div>
 </template>
 
 <script>
+import header from './components/header'
 import footernav from './components/footernav'
 export default {
   name: 'App',
   components: {
-    footernav
+    'v-header':header,
+   'v-foot':footernav
   }
 }
 </script>
@@ -21,5 +23,8 @@ export default {
 	padding: 0;
 	margin: 0;
 }
-
+#app{
+   font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+}
 </style>
