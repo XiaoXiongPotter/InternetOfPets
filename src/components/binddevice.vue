c2888d1b90e99432f27c54d067f2bb9a0034213e
daizhe <991655639@qq.com>
2018-08-12 10:10:18 +0800

    123

---
+++ src/components/binddevice.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="binddevice">
+		<p class="petdevice">绑定设备:{{this.device}}</p>
+	</div>
+</template>
+<script>
+import {getPetDevices} from'../deviceApi/index.js'
+export default {
+	name:'binddevice',
+	props:['id'],
+	data(){
+		return{
+			device:''
+		}
+	},
+	mounted(){
+		 let params = this.id
+			getPetDevices(params).then(res => {
+				if(res.data.header.status==1000){
+					this.device=res.data.data[0].devName
+				}
+     		}).catch(error => {
+     		console.log(error)
+     		})
+	}
+}	
+</script>
+<style scoped>
+	.petdevice{
+	padding-top: 30px;
+	font-family: "微软雅黑";
+	font-size: 14px;
+}
+</style>
---
+++ src/m_page/finder/guide.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="guide"> 
+    <header-top></header-top>
+    <div class="" ref="wrapper" >
+  <img src="../../image/guide.jpg" alt="">
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<br>
+    </div>
+  
+</div>
+</template>
+<script>
+import headerTop from "../../components/header.vue";
+import IScroll from 'iscroll/build/iscroll-probe'
+export default {
+  name: "guide",
+  data() {
+    return {};
+  },
+  created(){
+	 this.$nextTick(() => {
+          this.Scroll = new IScroll(this.$refs.wrapper, {
+          click: true
+        })
+       })     
+  },
+  components: {
+    headerTop
+  }
+};
+</script>
+<style scoped>
+.guide img {
+  width: 100%;
+}
+</style> 
