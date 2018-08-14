ab3dd47b116291a18946b472c0fa6a0016a794e0
wanghaiyang <619747158@qq.com>
2018-08-14 09:48:25 +0800

    协寻各个情况页

---
+++ src/m_page/finder/qrFindPet.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="qrFindFet"> 
+<header-top></header-top>
+this is qrfindpet
+  
+</div>
+</template>
+<script>
+import headerTop from "../../components/header.vue";
+export default {
+  name: "qrPindFet",
+  data() {
+    return {};
+  },
+  created(){    
+  },
+  components: {
+    headerTop
+  }
+};
+</script>
+<style scoped>
+
+
+</style> 
