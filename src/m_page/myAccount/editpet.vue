62c51d8bbabb84c4417395f231cda0696e0498f8
wanghaiyang <619747158@qq.com>
2018-08-15 10:39:30 +0800

    sadf

--- src/m_page/myAccount/editpet.vue
+++ src/m_page/myAccount/editpet.vue
@@ -1,81 +1,81 @@
 <template>
-	<transition name="show">
-		<div class="editpet" v-show="showflag">
-			<div class="header">
-				<img src="../../image/back.png" class="back" @click="back" />
-				<div class="imgBox"><img src="../../image/logo-m.png" alt=""></div>
-			</div>
-			<div class="title">
-				<div class="head_img">
-					<img :src="imgflag?avatar:avatar1" @click.stop="uploadHeadImg" style="margin: auto;display: block;" />
-				</div>
-				<input type="file" accept="image/*" @change="handleFile" class="hiddenInput" />
-				<p>点击头像可更换照片</p>
-			</div>
-			<div class="main" ref='wrapper'>
-				<div>
-					<div class="petmessage">
-						<div class="petname" v-if='list'>
-							<span>名称</span>
-							<el-input :value='petname' @input='change0'></el-input>
-						</div>
-						<div class="pet-msg">
-							<div class="petbelong" v-if='list'>
-								<span>种类</span>
-								<el-input :value='petbelong' @input='change1'></el-input>
-							</div>
-							<div class="sex" v-if='list'>
-								<span>性别</span>
-								<el-input :value='sex' @input='change2'></el-input>
-							</div>
-						</div>
-						<div class="pet-msg">
-							<div class="height" v-if='list'>
-								<span>身高(cm)</span>
-								<el-input :value='height' @input='change3'></el-input>
-							</div>
-							<div class="weight" v-if='list'>
-								<span>体重(kg)</span>
-								<el-input :value='weight' @input='change4'></el-input>
-							</div>
-						</div>
-						<div class="pet-msg">
-							<div class="birthday" v-if='list'>
-								<span>出生日期</span>
-								<div class="block">
-									<el-date-picker type="date" value-format="yyyy-MM-dd" v-model='birthday' @input='change5'>
-									</el-date-picker>
-								</div>
-							</div>
-							<div class="haircolor" v-if='list'>
-								<span>毛色</span>
-								<el-input :value='haircolor' @input='change6'></el-input>
-							</div>
-						</div>
-						<div class="character" v-if='list'>
-							<span>性格</span>
-							<el-input :value='character' @input='change7'></el-input>
-						</div>
-						<div class="binddevice" v-if='list'>
-							<span>绑定设备</span>
-							<p>{{this.device}}</p>
-						</div>
-					</div>
-					<div>
-						<div class="save-btn" style="margin: 20px 0;">
-							<el-button type="primary" round style='margin: auto;display: block;' @click='save'>保存修改</el-button>
-						</div>
-						<div class="delete-btn" style="margin: 20px 0;">
-							<el-button type="primary" round style='margin: auto;display: block;' @click='remove'>删除宠物</el-button>
-						</div>
-						<div class="search-btn" style="margin: 20px 0;">
-							<el-button type="primary" round style='margin: auto;display: block;' @click='search'>发布协寻</el-button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</transition>
+    <transition name="show">
+        <div class="editpet" v-show="showflag">
+            <div class="header">
+                <img src="../../image/back.png" class="back" @click="back" />
+                <div class="imgBox"><img src="../../image/logo-m.png" alt=""></div>
+            </div>
+            <div class="title">
+                <div class="head_img">
+                    <img :src="imgflag?avatar:avatar1" @click.stop="uploadHeadImg" style="margin: auto;display: block;" />
+                </div>
+                <input type="file" accept="image/*" @change="handleFile" class="hiddenInput" />
+                <p>点击头像可更换照片</p>
+            </div>
+            <div class="main" ref='wrapper'>
+                <div>
+                    <div class="petmessage">
+                        <div class="petname" v-if='list'>
+                            <span>名称</span>
+                            <el-input :value='petname' @input='change0'></el-input>
+                        </div>
+                        <div class="pet-msg">
+                            <div class="petbelong" v-if='list'>
+                                <span>种类</span>
+                                <el-input :value='petbelong' @input='change1'></el-input>
+                            </div>
+                            <div class="sex" v-if='list'>
+                                <span>性别</span>
+                                <el-input :value='sex' @input='change2'></el-input>
+                            </div>
+                        </div>
+                        <div class="pet-msg">
+                            <div class="height" v-if='list'>
+                                <span>身高(cm)</span>
+                                <el-input :value='height' @input='change3'></el-input>
+                            </div>
+                            <div class="weight" v-if='list'>
+                                <span>体重(kg)</span>
+                                <el-input :value='weight' @input='change4'></el-input>
+                            </div>
+                        </div>
+                        <div class="pet-msg">
+                            <div class="birthday" v-if='list'>
+                                <span>出生日期</span>
+                                <div class="block">
+                                    <el-date-picker type="date" value-format="yyyy-MM-dd" v-model='birthday' @input='change5'>
+                                    </el-date-picker>
+                                </div>
+                            </div>
+                            <div class="haircolor" v-if='list'>
+                                <span>毛色</span>
+                                <el-input :value='haircolor' @input='change6'></el-input>
+                            </div>
+                        </div>
+                        <div class="character" v-if='list'>
+                            <span>性格</span>
+                            <el-input :value='character' @input='change7'></el-input>
+                        </div>
+                        <div class="binddevice" v-if='list'>
+                            <span>绑定设备</span>
+                            <p>{{this.device}}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="save-btn" style="margin: 20px 0;">
+                            <el-button type="primary" round style='margin: auto;display: block;' @click='save'>保存修改</el-button>
+                        </div>
+                        <div class="delete-btn" style="margin: 20px 0;">
+                            <el-button type="primary" round style='margin: auto;display: block;' @click='remove'>删除宠物</el-button>
+                        </div>
+                        <div class="search-btn" style="margin: 20px 0;">
+                            <el-button type="primary" round style='margin: auto;display: block;' @click='search'>发布协寻</el-button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
 </template>
 <script>
 import IScroll from "iscroll/build/iscroll-probe";
@@ -116,6 +116,7 @@ export default {
             getPetDevices(params)
                 .then(res => {
                     if (res.data.header.status == 1000) {
+                        console.log(res)
                         this.device = res.data.data[0].device.type;
                     }
                 })
@@ -210,9 +211,13 @@ export default {
             this.showflag = true;
         },
         search() {
-            this.$router.replace({ path: "/releaseSearch" });
-            Bus.$emit("id", this.id);
-            Bus.$emit("petname", this.petname);
+            this.$router.push({
+                name: "releaseSearch",
+                query: {
+                    id: this.id,
+                    petname: this.petname
+                }
+            });
         },
         remove() {
             this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
@@ -281,7 +286,7 @@ export default {
     }
 };
 </script>
-
+ 
 <style>
 .el-message-box {
     width: 300px;
